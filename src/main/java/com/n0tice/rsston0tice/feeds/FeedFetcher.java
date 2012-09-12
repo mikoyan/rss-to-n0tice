4b38699a37e26b2daf7e86cbf944fe85d282d707
Tony McCrae <tony@eelpieconsulting.co.uk>
2012-09-12 13:40:18 +0100

    Add media rss dependency for extraction of image attachments.

--- src/main/java/com/n0tice/rsston0tice/feeds/FeedFetcher.java
+++ src/main/java/com/n0tice/rsston0tice/feeds/FeedFetcher.java
@@ -11,6 +11,12 @@ import org.springframework.stereotype.Component;
 import com.n0tice.rsston0tice.model.FeedItem;
 import com.sun.syndication.feed.module.georss.GeoRSSModule;
 import com.sun.syndication.feed.module.georss.GeoRSSUtils;
+import com.sun.syndication.feed.module.mediarss.MediaEntryModuleImpl;
+import com.sun.syndication.feed.module.mediarss.MediaModule;
+import com.sun.syndication.feed.module.mediarss.types.MediaContent;
+import com.sun.syndication.feed.module.mediarss.types.Metadata;
+import com.sun.syndication.feed.module.mediarss.types.Reference;
+import com.sun.syndication.feed.module.mediarss.types.Thumbnail;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.fetcher.impl.HttpURLFeedFetcher;
@@ -44,13 +50,15 @@ public class FeedFetcher {
         	
         	Double latitude = null;
         	Double longitude = null;        	
-			GeoRSSModule geoModule = (GeoRSSModule) GeoRSSUtils.getGeoRSS(syndEntry);
+			final GeoRSSModule geoModule = (GeoRSSModule) GeoRSSUtils.getGeoRSS(syndEntry);
 			if (geoModule != null && geoModule.getPosition() != null) {
 				latitude = geoModule.getPosition().getLatitude();
 				longitude = geoModule.getPosition().getLongitude();
 				log.debug("Rss item '" + syndEntry.getTitle() + "' has position information: " + latitude + "," + longitude);
 			}
         	
+			final String imageUrl = extractImage(syndEntry);
+			
         	final String body = syndEntry.getDescription() != null ? syndEntry.getDescription().getValue() : null;
 			feedItems.add(new FeedItem(syndEntry.getTitle(), syndEntry.getUri(), body, syndEntry.getLink(), syndEntry.getPublishedDate(), latitude, longitude));
         }
@@ -70,6 +78,45 @@ public class FeedFetcher {
 		} catch (Exception e) {
 			log.warn("Error while fetching feed: " + e.getMessage());
 		}
+		return null;
+	}
+	
+	private String extractImage(SyndEntry item) {
+		final MediaEntryModuleImpl mediaModule = (MediaEntryModuleImpl) item.getModule(MediaModule.URI);
+		if (mediaModule == null) {
+			log.debug("No media module found for item: " + item.getTitle());
+			return null;
+		}
+		
+		final MediaContent[] mediaContents = mediaModule.getMediaContents();
+		if (mediaContents.length > 0) {
+			MediaContent mediaContent = mediaContents[0];
+			
+			final Metadata metadata = mediaContent.getMetadata();
+			final Thumbnail[] thumbnails = metadata.getThumbnail();
+			if (thumbnails.length > 0) {
+				Thumbnail thumbnail = thumbnails[0];
+				log.info("Took first thumbnail on first mediaContent: " + thumbnail.getUrl());
+				return thumbnail.getUrl().toExternalForm();
+			}
+
+			if (mediaContent.getReference() != null) {
+				final Reference reference = mediaContent.getReference();
+				if (reference.toString().endsWith(".jpg")) {
+					log.info("Took image reference from first MediaContent: " + reference);
+				}
+				return reference.toString();
+			}
+			log.info(mediaContent.getReference());
+		}
+
+		final Thumbnail[] thumbnails = mediaModule.getMetadata().getThumbnail();
+		if (thumbnails.length > 0) {
+			Thumbnail thumbnail = thumbnails[0];
+			log.info("Took first thumbnail from module metadata: " + thumbnail.getUrl());
+			return thumbnail.getUrl().toExternalForm();
+		}
+		
 		return null;
 	}
 
